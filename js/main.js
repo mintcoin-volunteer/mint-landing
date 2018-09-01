c0f61dacb451fabd4b59d5ebf3d77e21e77a9a53
Jimmy Amash <jimmy@hellojumper.com>
2018-09-01 16:22:41 -0700

    Update (push)

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    $(".huh").click(function(){
+        $(".text-wrap-description").toggleClass("no-show");
+        $(".text-wrap-description .text-wrap-inner").toggleClass("fade-in fade-out");
+        $(".bottom-1").toggleClass("fade-in-social fade-out-social");
+        $(".bottom-1").removeClass("animate-first");
+    });
+    $(".join").click(function(){
+        $(".text-wrap-join").toggleClass("no-show");
+        $(".text-wrap-join .text-wrap-inner").toggleClass("fade-in fade-out");
+        $(".bottom-1").toggleClass("fade-in-social fade-out-social");
+        $(".bottom-1").removeClass("animate-first");
+    });
+});
